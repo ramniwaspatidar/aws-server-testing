610df97d7ab6af616c00be80c2ab73772b014570
Ram iOS <ramniwas.patidar@gmail.com>
2023-11-24 16:31:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 server.use(express.static(path.join(__dirname, 'public')));
 
 server.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 server.listen(port, () => {
