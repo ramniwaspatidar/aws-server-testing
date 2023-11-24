aec53cfc8313a76483c857a1542ab4456d68090f
Ram iOS <ramniwas.patidar@gmail.com>
2023-11-24 16:15:28 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const server = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the 'public' directory
+server.use(express.static(path.join(__dirname, 'public')));
+
+server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
